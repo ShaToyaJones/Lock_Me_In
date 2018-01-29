065a0981fb3803e9e79c44fb6780e55cef046859
ShaToya Jones <shatoyajones@ShaToyas-MacBook-Pro.local>
2018-01-28 21:39:00 -0500

    Added a search field on Client's page. Working on formatting. Need to work on authentication, finish the calendar, etc.

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,19 +5,19 @@ import "./SearchForm.css";
 const SearchForm = props =>
   <form className="search">
     <div className="form-group">
-      <label htmlFor="breed">Breed Name:</label>
+      <label htmlFor="clients">Clients Name:</label>
       <input
         value={props.search}
         onChange={props.handleInputChange}
-        name="breed"
-        list="breeds"
+        name="clients"
+        list="clients"
         type="text"
         className="form-control"
-        placeholder="Type in a dog breed to begin"
-        id="breed"
+        placeholder="Type in a client's name to begin:"
+        id="clients"
       />
-      <datalist id="breeds">
-        {props.breeds.map(breed => <option value={breed} key={breed} />)}
+      <datalist id="clients">
+        {props.clients.map(clients => <option value={clients} key={clients} />)}
       </datalist>
       <button
         type="submit"
--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,43 +1,58 @@
-import React from "react";
-import Hero from "../components/Hero";
+import React, { Component } from "react";
 import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+import Alert from "../components/Alert";
 
-const Clients = () =>
-  <div>
-    <Hero backgroundImage="https://cdn.pixabay.com/photo/2014/10/15/16/42/hairdresser-489915_960_720.jpg">
-      <h1>Clients Page</h1>
-      <h2>Search for your clients here!</h2>
-    </Hero>
-    <Container style={{ marginTop: 30 }}>
-      <Row>
-        <Col size="md-12">
-          <h1>Search for your clients!</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="md-12">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-            aliquet diam tortor, id consequat mauris ullamcorper eu. Orci varius
-            natoque penatibus et magnis dis parturient montes, nascetur
-            ridiculus mus. Pellentesque et dui id justo finibus sollicitudin at
-            et metus. 
-          </p>
-          <p>
-            Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum
-            porttitor. Sed malesuada molestie velit ac viverra. Quisque a.
-          </p>
-          <p>
-            Etiam ut massa efficitur, gravida sapien non, condimentum sapien.
-            Suspendisse massa tortor, facilisis in neque sit amet, scelerisque
-            elementum tortor. Nullam eget nibh sit amet odio lobortis
-            ullamcorper. 
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  </div>;
+class Clients extends Component {
+  state = {
+    search: "",
+    clients: [],
+    results: [],
+    error: ""
+  };
+
+  // // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  // componentDidMount() {
+  //   API.getBaseClientsList()
+  //     .then(res => this.setState({ clients: res.data.message }))
+  //     .catch(err => console.log(err));
+  // }
+
+  // handleInputChange = event => {
+  //   this.setState({ search: event.target.value });
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   API.getClientsName(this.state.search)
+  //     .then(res => {
+  //       if (res.data.status === "error") {
+  //         throw new Error(res.data.message);
+  //       }
+  //       this.setState({ results: res.data.message, error: "" });
+  //     })
+  //     .catch(err => this.setState({ error: err.message }));
+  // };
+  render() {
+    return (
+      <Container style={{ minHeight: "80%" }}>
+        <h1 className="text-center">Search By Client:</h1>
+        <Alert
+          type="danger"
+          style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+        >
+          {this.state.error}
+        </Alert>
+        <SearchForm
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+          clients={this.state.clients}
+        />
+        <SearchResults results={this.state.results} />
+      </Container>
+    );
+  }
+}
 
 export default Clients;
